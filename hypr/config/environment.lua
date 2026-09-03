59005779770cef6db8ccdf725c067f7e36e377f2
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2026-09-03 13:24:33 +0100

    Remap settings key and tune runtime env

--- hypr/config/environment.lua
+++ hypr/config/environment.lua
@@ -10,3 +10,16 @@ hl.env("LIBVA_DRIVER_NAME", "nvidia") -- Hardware acceleration on NVIDIA GPUs
 hl.env("__GL_GSYNC_ALLOWED", "1") -- Controls if G-Sync capable monitors should use Variable Refresh Rate (VRR)
 
 hl.env("AQ_DRM_DEVICES", "/dev/dri/nvidia:/dev/dri/intel")
+
+hl.env("GTK_IM_MODULE", "simple")
+
+hl.env("MANGOHUD", "1")
+hl.env("MANGOHUD_CONFIG", "no_display,fps_limit=120")
+
+hl.env("__GL_SHADER_DISK_CACHE_SIZE", "21474836480") -- 20GB
+hl.env("__GL_SHADER_DISK_CACHE_SKIP_CLEANUP", "1") -- Don't clean
+hl.env("MESA_SHADER_CACHE_MAX_SIZE", "20GB")
+
+hl.env("PROTON_USE_NTSYNC", "1")
+
+
